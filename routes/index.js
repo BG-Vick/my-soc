58a67f8ca95b2464a917c01689baf6be49129df5
Gennady Beryukaev <gberyukaev@a2seven.com>
2024-02-24 22:15:15 +0300

    likes controller and router added

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const { UserController, PostController, CommentController } = require('../controllers');
+const {
+  UserController,
+  PostController,
+  CommentController,
+  LikeController,
+} = require('../controllers');
 
 const { authenticateToken } = require('../middleware/auth');
 
@@ -29,5 +34,8 @@ router.delete('/posts/:id', authenticateToken, PostController.deletePost);
 
 router.post('/comments', authenticateToken, CommentController.createComment);
 router.delete('/comments/:id', authenticateToken, CommentController.deleteComment);
+
+router.post('/likes', authenticateToken, LikeController.likePost);
+router.delete('/likes/:id', authenticateToken, LikeController.unlikePost);
 
 module.exports = router;
