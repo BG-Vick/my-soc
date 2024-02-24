c08e5347936e70147ae204b4c8898170ef6e01a0
Gennady Beryukaev <gberyukaev@a2seven.com>
2024-02-24 21:20:52 +0300

    create Post method added

--- controllers/post-controller.js
+++ controllers/post-controller.js
@@ -1,6 +1,28 @@
+const { prisma } = require('../prisma/prisma-client');
+
 const PostController = {
   createPost: async (req, res) => {
-    res.send('createPost');
+    const { content } = req.body;
+    const authorId = req.user.userId;
+
+    if (!content) {
+      return res.status(400).json({ error: 'Все поля обязательны' });
+    }
+
+    try {
+      const post = await prisma.post.create({
+        data: {
+          content,
+          authorId,
+        },
+      });
+      console.log(post);
+      res.json(post);
+    } catch (error) {
+      console.error('Error in createPost:', error);
+
+      res.status(500).json({ error: 'Internal server error' });
+    }
   },
 
   getAllPosts: async (req, res) => {
